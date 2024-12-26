c3378a3e80daed1afac62002c8d42d093e85720d
Saadia Bouloudene <saadiabouloudene@gmail.com>
2024-12-26 14:11:32 +0100

    resolution conflits

--- tests/test_bank_latest.py
+++ tests/test_bank_latest.py
@@ -22,7 +22,6 @@ def valid_account(db_session):
     db_session.commit()
     return account
 
-
 # Classe pour les tests
 class TestBank:
     def test_account_valid(self, valid_account, db_session):
